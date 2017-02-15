637eb7b03c43c623b90d2600fa8e21d8111449f9
kingcos <2821836721v@gmail.com>
2017-02-15 20:25:13 +0800

    Add video 01 code

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -4,22 +4,58 @@
 //
 //  Created by 买明 on 15/02/2017.
 //  Copyright © 2017 买明. All rights reserved.
-//
+//  [Powered by kingcos](https://github.com/kingcos/CS193P_2017)
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var display: UILabel!
+    
+    // 存储属性
+    var userIsInTheMiddleOfTyping = false
+    
+    // 计算属性
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        
+        set {
+            display.text = String(newValue)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        
+        let digit = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+        
     }
-
-
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        
+        userIsInTheMiddleOfTyping = false
+        
+        if let mathematicalSymbol = sender.currentTitle {
+            switch mathematicalSymbol {
+            case "π":
+                displayValue = Double.pi
+            case "√":
+                displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+        }
+        
+    }
+    
 }
 
