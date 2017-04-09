3b85479d2bd825df3bd0bd39cd2837c233e28974
kingcos <2821836721v@gmail.com>
2017-04-09 22:52:12 +0800

    Add video 10 code

---
+++ CoreDataExample/CoreDataExample/TwitterUser.swift
@@ -0,0 +1,14 @@
+//
+//  TwitterUser.swift
+//  CoreDataExample
+//
+//  Created by 买明 on 19/03/2017.
+//  Copyright © 2017 买明. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TwitterUser: NSManagedObject {
+
+}
---
+++ CoreDataExample/CoreDataExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataExample
+//
+//  Created by 买明 on 19/03/2017.
+//  Copyright © 2017 买明. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
