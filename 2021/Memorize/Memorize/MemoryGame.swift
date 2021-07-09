69eca349e8ed4bb1b8fbbdd15887462c135bcf26
kingcos <2821836721v@gmail.com>
2021-07-09 20:51:50 +0800

    Add more logics

--- 2021/Memorize/Memorize/MemoryGame.swift
+++ 2021/Memorize/Memorize/MemoryGame.swift
@@ -9,7 +9,7 @@ import Foundation
 
 // Model Layer
 
-struct MemoryGame<CardContent> {
+struct MemoryGame<CardContent> where CardContent: Equatable {
     struct Card: Identifiable {
         let id: Int
         
@@ -19,6 +19,7 @@ struct MemoryGame<CardContent> {
     }
     
     private(set) var cards: [Card] = []
+    private var indexOfTheOneAndOnlyFaceUpCard: Int?
     
     init(numberOfPairsOfCards: Int, _ createCardContent: (Int) -> CardContent) {
         for pairIndex in 0..<numberOfPairsOfCards {
@@ -29,8 +30,26 @@ struct MemoryGame<CardContent> {
     }
     
     mutating func choose(_ card: Card) {
-        if let choseIndex = cards.firstIndex(where: { $0.id == card.id }) {
-            cards[choseIndex].isFaceUp.toggle()
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }),
+           !cards[chosenIndex].isFaceUp,
+           !cards[chosenIndex].isMatched {
+            if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard {
+                // 当前已经有一张被选中
+                if cards[chosenIndex].content == cards[potentialMatchIndex].content {
+                    cards[chosenIndex].isMatched = true
+                    cards[potentialMatchIndex].isMatched = true
+                }
+                
+                indexOfTheOneAndOnlyFaceUpCard = nil
+            } else {
+                // 刚选择第一张（归位其他）
+                for index in cards.indices {
+                    cards[index].isFaceUp = false
+                }
+                indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+            }
+            
+            cards[chosenIndex].isFaceUp.toggle()
         }
     }
 }
